8fcada7245fd85007e05eaa92129f4c1bad71c20
Riad Afridi Shibly <riad@evatix.com>
2022-08-11 15:10:36 +0600

    Added navbar, Announcement and remove not needed files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,91 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 60px;
+  /* background-color: lightpink; */
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+`;
+
+const Language = styled.span`
+  font-size: 14;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 0.5px solid lightgray;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  opacity: 0.5;
+`;
+
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="Search..." />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>Wopsie.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Log In</MenuItem>
+          <MenuItem>Sign Up</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
